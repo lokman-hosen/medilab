acb250d30156e20f341b1a224a420cf827d1688f
lokman_hosen <lokman.cse.pstu@gmail.com>
2021-06-18 21:24:56 +0600

    working on home page

--- src/components/mainContent.js
+++ src/components/mainContent.js
@@ -1,17 +1,12 @@
 import React, { useState } from 'react';
+import WhyUs from "./whyUs";
 
-function Hero() {
+function MainContent() {
     return (
         <div>
-            <section id="hero" className="d-flex align-items-center">
-                <div className="container">
-                    <h1>Welcome to Medilab</h1>
-                    <h2>We are team of talented designers making websites with Bootstrap</h2>
-                    <a href="#about" className="btn-get-started scrollto">Get Started</a>
-                </div>
-            </section>
+            <WhyUs />
         </div>
     );
 }
 
-export default Hero
+export default MainContent
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,27 +1,48 @@
 import React, { useState } from 'react';
 
-function Topbar() {
-    // Declare a new state variable, which we'll call "count"
-    const [count, setCount] = useState(0);
-
+function Navbar() {
     return (
         <div>
-            <div id="topbar" className="d-flex align-items-center fixed-top">
-                <div className="container d-flex justify-content-between">
-                    <div className="contact-info d-flex align-items-center">
-                        <i className="bi bi-envelope"></i> <a href="mailto:contact@example.com">contact@example.com</a>
-                        <i className="bi bi-phone"></i> +1 5589 55488 55
-                    </div>
-                    <div className="d-none d-lg-flex social-links align-items-center">
-                        <a href="#" className="twitter"><i className="bi bi-twitter"></i></a>
-                        <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
-                        <a href="#" className="instagram"><i className="bi bi-instagram"></i></a>
-                        <a href="#" className="linkedin"><i className="bi bi-linkedin"></i></a>
+            <header id="header" className="fixed-top">
+                <div className="container d-flex align-items-center">
+                    <h1 className="logo me-auto"><a href="index.html">Medilab</a></h1>
+                    <nav id="navbar" className="navbar order-last order-lg-0">
+                        <ul>
+                            <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
+                            <li><a className="nav-link scrollto" href="#about">About</a></li>
+                            <li><a className="nav-link scrollto" href="#services">Services</a></li>
+                            <li><a className="nav-link scrollto" href="#departments">Departments</a></li>
+                            <li><a className="nav-link scrollto" href="#doctors">Doctors</a></li>
+                            <li className="dropdown">
+                                <a href="#"><span>Drop Down</span>
+                                <i className="bi bi-chevron-down"></i></a>
+                                <ul>
+                                    <li><a href="#">Drop Down 1</a></li>
+                                    <li className="dropdown"><a href="#"><span>Deep Drop Down</span>
+                                        <i className="bi bi-chevron-right"></i></a>
+                                        <ul>
+                                            <li><a href="#">Deep Drop Down 1</a></li>
+                                            <li><a href="#">Deep Drop Down 2</a></li>
+                                            <li><a href="#">Deep Drop Down 3</a></li>
+                                            <li><a href="#">Deep Drop Down 4</a></li>
+                                            <li><a href="#">Deep Drop Down 5</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Drop Down 2</a></li>
+                                    <li><a href="#">Drop Down 3</a></li>
+                                    <li><a href="#">Drop Down 4</a></li>
+                                </ul>
+                            </li>
+                            <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
+                        </ul>
+                        <i className="bi bi-list mobile-nav-toggle"></i>
+                    </nav>
+                    <a href="#appointment" className="appointment-btn scrollto"><span className="d-none d-md-inline">Make an</span> Appointment</a>
+
                 </div>
-            </div>
-        </div>
+            </header>
         </div>
     );
 }
 
-export default Topbar
+export default Navbar
