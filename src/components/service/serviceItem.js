06c57f149876fb4d6c44d2cfdb61ae74d09cf56c
lokman_hosen <lokman.cse.pstu@gmail.com>
2021-06-25 16:42:38 +0600

    Service section updated

--- src/components/service/serviceItem.js
+++ src/components/service/serviceItem.js
@@ -1,55 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 
 function ServiceItem() {
+    const initialValue = [
+        {
+            id: 1,
+            text: "Lorem Ipsum",
+            description: "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi",
+            icon: "fa-heartbeat"
+        },
+        {
+            id: 2,
+            text: "Sed ut perspiciatis",
+            description: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
+            icon: "fa-pills"
+        },
+        {
+            id: 3,
+            text: "Magni Dolores",
+            description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia",
+            icon: "fa-hospital-user"
+        },
+        {
+            id: 3,
+            text: "Nemo Enim",
+            description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis",
+            icon: "fa-dna"
+        },
+        {
+            id: 3,
+            text: "Dele cardo",
+            description: "Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur",
+            icon: "fa-wheelchair"
+        },
+        {
+            id: 3,
+            text: "Divera don",
+            description: "Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur",
+            icon: "fa-notes-medical"
+        }
+    ]
+    const [ services, setMovies ] = useState(initialValue);
+
+
     return (
         <div className="row">
-            <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
-                <div className="icon-box">
-                    <div className="icon"><i className="fas fa-heartbeat"></i></div>
-                    <h4><a href="">Lorem Ipsum</a></h4>
-                    <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
-                </div>
-            </div>
+            {services.map((service) => {
+                return <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" key={service.id}>
+                        <div className="icon-box">
+                            <div className="icon"><i className={`fas ${service.icon}`}></i></div>
+                            <h4><a href="">{service.text}</a></h4>
+                            <p>{service.description}</p>
+                        </div>
+                    </div>;
+            })}
 
-            <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-                <div className="icon-box">
-                    <div className="icon"><i className="fas fa-pills"></i></div>
-                    <h4><a href="">Sed ut perspiciatis</a></h4>
-                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-                </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
-                <div className="icon-box">
-                    <div className="icon"><i className="fas fa-hospital-user"></i></div>
-                    <h4><a href="">Magni Dolores</a></h4>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
-                </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-                <div className="icon-box">
-                    <div className="icon"><i className="fas fa-dna"></i></div>
-                    <h4><a href="">Nemo Enim</a></h4>
-                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
-                </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-                <div className="icon-box">
-                    <div className="icon"><i className="fas fa-wheelchair"></i></div>
-                    <h4><a href="">Dele cardo</a></h4>
-                    <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
-                </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-                <div className="icon-box">
-                    <div className="icon"><i className="fas fa-notes-medical"></i></div>
-                    <h4><a href="">Divera don</a></h4>
-                    <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
-                </div>
-            </div>
         </div>
     );
 }
